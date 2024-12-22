7d4411a3d67c886aebf779e2640c902ca66daf9c
Simon Martinelli <simon@martinelli.ch>
2024-12-22 17:40:14 +0100

    Updated JavaScript

--- src/main/java/ch/jtaf/ui/component/GoogleAnalytics.java
+++ src/main/java/ch/jtaf/ui/component/GoogleAnalytics.java
@@ -3,35 +3,49 @@ package ch.jtaf.ui.component;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.Tag;
+import elemental.json.JsonObject;
 
 @Tag("google-analytics")
 public class GoogleAnalytics extends Component implements HasSize {
 
     public GoogleAnalytics(String measurementId) {
-        getElement().executeJs(
-            """
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','$0');
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '$0');
-                """, measurementId
+        getElement().executeJs("""
+            (function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0];
+                var j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', '$0');
+            window.dataLayer = window.dataLayer || [];
+            
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            
+            gtag('js', new Date());
+            gtag('config', '$0');
+            """, measurementId
         );
     }
 
     public void sendPageView(String pageName) {
+        getElement().executeJs("""
+            gtag('event', 'page_view', {
+                'page_title': $0,
+                'page_location': window.location.href,
+                'page_path': window.location.pathname
+            });
+            """, pageName
+        );
+    }
+
+    public void sendEvent(String eventName, JsonObject eventParams) {
         getElement().executeJs(
-            """
-                gtag('event', 'page_view', {
-                    'page_title': $0,
-                    'page_location': window.location.href,
-                    'page_path': window.location.pathname
-                });
-                """, pageName
+            "gtag('event', $0, $1);",
+            eventName, eventParams
         );
     }
 }
